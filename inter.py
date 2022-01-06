f2eb26b43367c30fa4d5b40cceaa025ee9d1c62f
unknown <smail.tache@univ-constantine2.dz>
2022-01-06 09:01:23 +0100

    initial commit

---
+++ inter.py
@@ -0,0 +1,12 @@
+import kivy
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+
+class ccc(App):
+
+    def build(self):
+        return BoxLayout()
+
+c = ccc()
+c.run()
